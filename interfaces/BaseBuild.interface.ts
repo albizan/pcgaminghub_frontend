c5d69c01ddca37b2b3c9054ad6a0297651bc80d2
unknown <albizan@hotmail.it>
2020-09-02 13:12:45 +0200

    Add BaseBuild interface

---
+++ interfaces/BaseBuild.interface.ts
@@ -0,0 +1,10 @@
+export interface BaseBuild {
+  id: string;
+  price: number;
+  name: string;
+  cpuBrand: string;
+  gpuBrand: string;
+  cpuLabel: string;
+  gpuLabel: string;
+  date: string;
+}
