9d5e3c2f1e53cc80b7002efc514d10072934f041
unknown <albizan@hotmail.it>
2020-09-02 22:39:34 +0200

    Add baseUrls

--- utils/http.ts
+++ utils/http.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const baseUrls = {
   development: "http://localhost:80/api",
-  production: "http://localhost:80/api",
+  production: "https://067cc9c1e1e7.ngrok.io/api",
 };
 
 export const http = axios.create({
-  baseURL: "http://localhost:80/api",
+  baseURL: baseUrls[process.env.NODE_ENV || "development"],
   timeout: 2500,
 });
