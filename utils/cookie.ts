1c4c9814956c7ac9ce4cb27164135b05824f204b
unknown <albizan@hotmail.it>
2020-09-03 20:11:39 +0200

    Add cookie helpers

---
+++ utils/cookie.ts
@@ -0,0 +1,13 @@
+import Cookies from "universal-cookie";
+import { NextPageContext } from "next";
+
+export function setAuthCookie(accessToken: string) {
+  const cookies = new Cookies();
+  cookies.set("accessToken", accessToken, { path: "/" });
+}
+
+export function checkAuthCookie(context: NextPageContext) {
+  const cookies = new Cookies(context.req ? context.req.headers.cookie : null);
+  const accessToken = cookies.get("accessToken") || "";
+  return accessToken;
+}
