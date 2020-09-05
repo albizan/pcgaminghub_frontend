2d1b9a691635396958902efe59ad3676b11a5abe
unknown <albizan@hotmail.it>
2020-09-05 14:39:02 +0200

    Export a base instance without interceptors

--- utils/http.ts
+++ utils/http.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
+import { getAccessToken } from "./auth";
 
 const baseUrls = {
   development: "http://localhost:80/api",
   production: "https://8d649e6ca716.ngrok.io/api",
 };
 
-export const http = axios.create({
+const instance = axios.create({
   baseURL: baseUrls[process.env.NODE_ENV || "development"],
   timeout: 2500,
 });
+
+export const baseInstance = axios.create({
+  baseURL: baseUrls[process.env.NODE_ENV || "development"],
+  timeout: 2500,
+});
+
+instance.interceptors.request.use((reqConfig) => {
+  if (localStorage) {
+    const accessToken: string = getAccessToken() || "";
+    reqConfig.headers.authorization = "Bearer " + accessToken;
+  }
+
+  return reqConfig;
+});
+
+export const http = instance;
