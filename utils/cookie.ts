66b5b3e4b78750c9d57dabb6f0cb338f8b4f0305
unknown <albizan@hotmail.it>
2020-09-05 14:16:00 +0200

    removeCookie

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -6,6 +6,11 @@ export function setAuthCookie(accessToken: string) {
   cookies.set("accessToken", accessToken, { path: "/" });
 }
 
+export function removeAuthCookie() {
+  const cookies = new Cookies();
+  cookies.remove("accessToken");
+}
+
 export function checkAuthCookie(context: NextPageContext) {
   const cookies = new Cookies(context.req ? context.req.headers.cookie : null);
   const accessToken = cookies.get("accessToken") || "";
