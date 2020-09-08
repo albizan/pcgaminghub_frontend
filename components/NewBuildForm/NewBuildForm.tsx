a51e0318e6c046fa7d4927696838a35b313f3113
unknown <albizan@hotmail.it>
2020-09-08 14:28:06 +0200

    Display error messages

--- components/NewBuildForm/NewBuildForm.tsx
+++ components/NewBuildForm/NewBuildForm.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { http } from "../../utils/http";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAccessToken } from "../../utils/auth";
 
 export default function NewBuild() {
@@ -9,8 +9,9 @@ export default function NewBuild() {
     const token = localStorage ? getAccessToken() : "";
     if (!token) router.push("/login");
   });
+  const [message, setMessage] = useState<string>();
   const router = useRouter();
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm({ mode: "onBlur" });
   const onSubmit = async (data) => {
     try {
       const result = await http.post("/build/new", {
@@ -56,118 +57,262 @@ export default function NewBuild() {
           label: data.coolerLabel,
         },
       });
-      console.log(result);
       if (result.status) {
         router.push("/dashboard");
       }
     } catch (error) {
-      console.log(error);
+      setMessage("Si è verificato un errore imprevisto");
     }
   };
-  console.log(errors);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <p>Prezzo</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="Prezzo" name="price" ref={register({ required: true })} />
+    <form className="text-gray-800 font-semibold px-4" onSubmit={handleSubmit(onSubmit)}>
+      {message && <p className="text-red-400 text-center text-2xl my-6">{message}</p>}
+      <h2 className="text-xl md:text-4xl mb-8">Nuova configurazione</h2>
+      <p className="text-lg mt-8">Prezzo</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <input
+          className="flex-1 mt-4 lg:mt-0 p-4 lg:mx-2 border rounded border-gray-400"
+          type="text"
+          placeholder="Prezzo"
+          name="price"
+          ref={register({ required: "Inserire un prezzo" })}
+        />
+      </div>
+      {errors.price && <span className="text-red-400 text-sm">{errors.price.message}</span>}
+
+      <p className="text-lg mt-8">Nome</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <input
+          className="flex-1 mt-4 lg:mt-0 p-4 lg:mx-2 border rounded border-gray-400"
+          type="text"
+          placeholder="Nome"
+          name="name"
+          ref={register({ required: "Inserire un nome" })}
+        />
+      </div>
+      {errors.name && <span className="text-red-400 text-sm">{errors.name.message}</span>}
+
+      <p className="text-lg mt-8">Descrizione</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <input
+          className="flex-1 mt-4 lg:mt-0 p-4 lg:mx-2 border rounded border-gray-400"
+          type="text"
+          placeholder="Descrizione"
+          name="description"
+          ref={register({ required: false })}
+        />
       </div>
 
-      <p>Nome</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="Nome" name="name" ref={register({ required: true })} />
-      </div>
-
-      <p>Descrizione</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="Descrizione" name="description" ref={register({ required: false })} />
-      </div>
-
-      <p>Brand Processore</p>
-      <select name="cpuBrand" ref={register({ required: true })}>
+      <p className="text-lg mt-8">Brand Processore</p>
+      <select className="border border-gray-500 rounded px-2 py-1 focus:outline-none" name="cpuBrand" ref={register({ required: "Brand Processore mancante" })}>
+        <option value=""></option>
         <option value="AMD">AMD</option>
         <option value="INTEL">INTEL</option>
       </select>
+      {errors.cpuBrand && <span className="block text-red-400 text-sm">{errors.cpuBrand.message}</span>}
 
-      <p>Brand Scheda Video</p>
-      <select name="gpuBrand" ref={register({ required: true })}>
+      <p className="text-lg mt-8">Brand Scheda Video</p>
+      <select className="border border-gray-500 rounded px-2 py-1 focus:outline-none" name="gpuBrand" ref={register({ required: "Brand Scheda Video mancante" })}>
+        <option value=""></option>
         <option value="AMD">AMD</option>
         <option value="NVIDIA">NVIDIA</option>
       </select>
+      {errors.gpuBrand && <span className="block text-red-400 text-sm">{errors.gpuBrand.message}</span>}
 
-      <p>Processore</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="CPU ASIN" name="cpuAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL CPU" name="cpuLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Processore</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="cpuAsin"
+            ref={register({ required: "Inserire ASIN processore", minLength: 10, maxLength: 10 })}
+          />
+          {errors.cpuAsin && <span className="block text-red-400 text-sm">{errors.cpuAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="cpuLabel"
+            ref={register({ required: "Inserire label processore" })}
+          />
+          {errors.cpuLabel && <span className="block text-red-400 text-sm">{errors.cpuLabel.message}</span>}
+        </div>
       </div>
 
-      <p>Scheda Video</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="GPU ASIN" name="gpuAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL GPU" name="gpuLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Scheda Video</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="gpuAsin"
+            ref={register({ required: "Inserire ASIN scheda video", minLength: 10, maxLength: 10 })}
+          />
+          {errors.gpuAsin && <span className="block text-red-400 text-sm">{errors.gpuAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="gpuLabel"
+            ref={register({ required: "Inserire label scheda video" })}
+          />
+          {errors.gpuLabel && <span className="block text-red-400 text-sm">{errors.gpuLabel.message}</span>}
+        </div>
       </div>
 
-      <p>Alimentatore</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="PSU ASIN" name="psuAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL PSU" name="psuLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Alimentatore</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="psuAsin"
+            ref={register({ required: "Inserire ASIN alimentatore", minLength: 10, maxLength: 10 })}
+          />
+          {errors.psuAsin && <span className="block text-red-400 text-sm">{errors.psuAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="psuLabel"
+            ref={register({ required: "Inserire label alimentatore" })}
+          />
+          {errors.psuLabel && <span className="block text-red-400 text-sm">{errors.psuLabel.message}</span>}
+        </div>
       </div>
 
-      <p>Scheda Madre</p>
-      <div className="w-full flex my-3">
-        <input
-          className="flex-1 p-2 border rounded"
-          type="text"
-          placeholder="MOBO ASIN"
-          name="moboAsin"
-          ref={register({ required: true, minLength: 10, maxLength: 10 })}
-        />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL MOBO" name="moboLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Scheda Madre</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="moboAsin"
+            ref={register({ required: "Inserire ASIN scheda madre", minLength: 10, maxLength: 10 })}
+          />
+          {errors.moboAsin && <span className="block text-red-400 text-sm">{errors.moboAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="moboLabel"
+            ref={register({ required: "Inserire label scheda madre" })}
+          />
+          {errors.moboLabel && <span className="block text-red-400 text-sm">{errors.moboLabel.message}</span>}
+        </div>
       </div>
 
-      <p>Case</p>
-      <div className="w-full flex my-3">
-        <input
-          className="flex-1 p-2 border rounded"
-          type="text"
-          placeholder="Case ASIN"
-          name="caseAsin"
-          ref={register({ required: true, minLength: 10, maxLength: 10 })}
-        />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL Case" name="caseLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Case</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="caseAsin"
+            ref={register({ required: "Inserire ASIN case", minLength: 10, maxLength: 10 })}
+          />
+          {errors.caseAsin && <span className="block text-red-400 text-sm">{errors.caseAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="caseLabel"
+            ref={register({ required: "Inserire label case" })}
+          />
+          {errors.caseLabel && <span className="block text-red-400 text-sm">{errors.caseLabel.message}</span>}
+        </div>
       </div>
 
-      <p>RAM</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="RAM ASIN" name="ramAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL RAM" name="ramLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">RAM</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="ramAsin"
+            ref={register({ required: "Inserire ASIN RAM", minLength: 10, maxLength: 10 })}
+          />
+          {errors.ramAsin && <span className="block text-red-400 text-sm">{errors.ramAsin.message}</span>}
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="Label"
+            name="ramLabel"
+            ref={register({ required: "Inserire label RAM" })}
+          />
+          {errors.ramLabel && <span className="block text-red-400 text-sm">{errors.ramLabel.message}</span>}
+        </div>
       </div>
 
-      <p>SSD</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="ssd ASIN" name="ssdAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL ssd" name="ssdLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">SSD</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="ssdAsin"
+            ref={register({ required: false, minLength: 10, maxLength: 10 })}
+          />
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="ssdLabel" ref={register()} />
+        </div>
       </div>
 
-      <p>HDD</p>
-      <div className="w-full flex my-3">
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="hdd ASIN" name="hddAsin" ref={register({ required: true, minLength: 10, maxLength: 10 })} />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL hdd" name="hddLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">HDD</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="hddAsin"
+            ref={register({ required: false, minLength: 10, maxLength: 10 })}
+          />
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="hddLabel" ref={register()} />
+        </div>
       </div>
 
-      <p>Dissipatore</p>
-      <div className="w-full flex my-3">
-        <input
-          className="flex-1 p-2 border rounded"
-          type="text"
-          placeholder="cooler ASIN"
-          name="coolerAsin"
-          ref={register({ required: true, minLength: 10, maxLength: 10 })}
-        />
-        <input className="flex-1 p-2 border rounded" type="text" placeholder="LABEL cooler" name="coolerLabel" ref={register({ required: true })} />
+      <p className="text-lg mt-8">Dissipatore</p>
+      <div className="w-full flex flex-col lg:flex-row my-3">
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input
+            className="w-full p-4 border rounded border-gray-400"
+            type="text"
+            placeholder="ASIN"
+            name="coolerAsin"
+            ref={register({ required: false, minLength: 10, maxLength: 10 })}
+          />
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="coolerLabel" ref={register()} />
+        </div>
       </div>
 
-      <button className="gradient text-white px-4 py-1 uppercase text-lg rounded" type="submit">
+      <button className="gradient text-white px-5 py-3 uppercase text-2xl w-full rounded mt-8" type="submit">
         Invia
       </button>
     </form>
