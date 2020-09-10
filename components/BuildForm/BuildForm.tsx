a7b06d5daad403de101ef87689b0476ed1e48977
unknown <albizan@hotmail.it>
2020-09-10 21:24:21 +0200

    Changed name

--- components/BuildForm/BuildForm.tsx
+++ components/BuildForm/BuildForm.tsx
@@ -4,59 +4,17 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getAccessToken } from "../../utils/auth";
 
-export default function NewBuild() {
+export default function BuildForm({ defaultValues, buildId }) {
   useEffect(() => {
     const token = localStorage ? getAccessToken() : "";
     if (!token) router.push("/login");
-  });
+  }, []);
   const [message, setMessage] = useState<string>();
   const router = useRouter();
-  const { register, handleSubmit, errors } = useForm({ mode: "onBlur" });
+  const { register, handleSubmit, errors } = useForm({ mode: "onBlur", defaultValues: defaultValues || {} });
   const onSubmit = async (data) => {
     try {
-      const result = await http.post("/build/new", {
-        price: data.price,
-        name: data.name,
-        cpuBrand: data.cpuBrand,
-        gpuBrand: data.gpuBrand,
-        description: data.description,
-        CPU: {
-          asin: data.cpuAsin,
-          label: data.cpuLabel,
-        },
-        "Scheda Madre": {
-          asin: data.moboAsin,
-          label: data.moboLabel,
-        },
-        Alimentatore: {
-          asin: data.psuAsin,
-          label: data.psuLabel,
-        },
-        Case: {
-          asin: data.caseAsin,
-          label: data.caseLabel,
-        },
-        RAM: {
-          asin: data.ramAsin,
-          label: data.ramLabel,
-        },
-        GPU: {
-          asin: data.gpuAsin,
-          label: data.gpuLabel,
-        },
-        SSD: {
-          asin: data.ssdAsin,
-          label: data.ssdLabel,
-        },
-        HDD: {
-          asin: data.hddAsin,
-          label: data.hddLabel,
-        },
-        Dissipatore: {
-          asin: data.coolerAsin,
-          label: data.coolerLabel,
-        },
-      });
+      const result = await http.post("/build/new", data);
       if (result.status) {
         router.push("/dashboard");
       }
@@ -67,8 +25,9 @@ export default function NewBuild() {
 
   return (
     <form className="text-gray-800 font-semibold px-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-gray-800 text-xl lg:text-3xl">{buildId ? "Modifica configurazione" : "Nuova Configurazione"}</h1>
+      {buildId && <p className="text-sm text-gray-600">{buildId}</p>}
       {message && <p className="text-red-400 text-center text-2xl my-6">{message}</p>}
-      <h2 className="text-xl md:text-4xl mb-8">Nuova configurazione</h2>
       <p className="text-lg mt-8">Prezzo</p>
       <div className="w-full flex flex-col lg:flex-row my-3">
         <input
@@ -127,7 +86,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="cpuAsin"
+            name="CPU.asin"
             ref={register({ required: "Inserire ASIN processore", minLength: 10, maxLength: 10 })}
           />
           {errors.cpuAsin && <span className="block text-red-400 text-sm">{errors.cpuAsin.message}</span>}
@@ -137,7 +96,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="cpuLabel"
+            name="CPU.label"
             ref={register({ required: "Inserire label processore" })}
           />
           {errors.cpuLabel && <span className="block text-red-400 text-sm">{errors.cpuLabel.message}</span>}
@@ -151,7 +110,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="gpuAsin"
+            name="GPU.asin"
             ref={register({ required: "Inserire ASIN scheda video", minLength: 10, maxLength: 10 })}
           />
           {errors.gpuAsin && <span className="block text-red-400 text-sm">{errors.gpuAsin.message}</span>}
@@ -161,7 +120,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="gpuLabel"
+            name="GPU.label"
             ref={register({ required: "Inserire label scheda video" })}
           />
           {errors.gpuLabel && <span className="block text-red-400 text-sm">{errors.gpuLabel.message}</span>}
@@ -175,7 +134,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="psuAsin"
+            name="Alimentatore.asin"
             ref={register({ required: "Inserire ASIN alimentatore", minLength: 10, maxLength: 10 })}
           />
           {errors.psuAsin && <span className="block text-red-400 text-sm">{errors.psuAsin.message}</span>}
@@ -185,7 +144,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="psuLabel"
+            name="Alimentatore.label"
             ref={register({ required: "Inserire label alimentatore" })}
           />
           {errors.psuLabel && <span className="block text-red-400 text-sm">{errors.psuLabel.message}</span>}
@@ -199,7 +158,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="moboAsin"
+            name="Scheda Madre.asin"
             ref={register({ required: "Inserire ASIN scheda madre", minLength: 10, maxLength: 10 })}
           />
           {errors.moboAsin && <span className="block text-red-400 text-sm">{errors.moboAsin.message}</span>}
@@ -209,7 +168,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="moboLabel"
+            name="Scheda Madre.label"
             ref={register({ required: "Inserire label scheda madre" })}
           />
           {errors.moboLabel && <span className="block text-red-400 text-sm">{errors.moboLabel.message}</span>}
@@ -223,7 +182,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="caseAsin"
+            name="Case.asin"
             ref={register({ required: "Inserire ASIN case", minLength: 10, maxLength: 10 })}
           />
           {errors.caseAsin && <span className="block text-red-400 text-sm">{errors.caseAsin.message}</span>}
@@ -233,7 +192,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="caseLabel"
+            name="Case.label"
             ref={register({ required: "Inserire label case" })}
           />
           {errors.caseLabel && <span className="block text-red-400 text-sm">{errors.caseLabel.message}</span>}
@@ -247,7 +206,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="ramAsin"
+            name="RAM.asin"
             ref={register({ required: "Inserire ASIN RAM", minLength: 10, maxLength: 10 })}
           />
           {errors.ramAsin && <span className="block text-red-400 text-sm">{errors.ramAsin.message}</span>}
@@ -257,7 +216,7 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="Label"
-            name="ramLabel"
+            name="RAM.label"
             ref={register({ required: "Inserire label RAM" })}
           />
           {errors.ramLabel && <span className="block text-red-400 text-sm">{errors.ramLabel.message}</span>}
@@ -271,12 +230,12 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="ssdAsin"
+            name="SSD.asin"
             ref={register({ required: false, minLength: 10, maxLength: 10 })}
           />
         </div>
         <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
-          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="ssdLabel" ref={register()} />
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="SSD.label" ref={register()} />
         </div>
       </div>
 
@@ -287,12 +246,12 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="hddAsin"
+            name="HDD.asin"
             ref={register({ required: false, minLength: 10, maxLength: 10 })}
           />
         </div>
         <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
-          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="hddLabel" ref={register()} />
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="HDD.label" ref={register()} />
         </div>
       </div>
 
@@ -303,12 +262,12 @@ export default function NewBuild() {
             className="w-full p-4 border rounded border-gray-400"
             type="text"
             placeholder="ASIN"
-            name="coolerAsin"
+            name="Dissipatore.asin"
             ref={register({ required: false, minLength: 10, maxLength: 10 })}
           />
         </div>
         <div className="flex-1 mt-4 lg:mt-0 lg:mx-2">
-          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="coolerLabel" ref={register()} />
+          <input className="w-full p-4 border rounded border-gray-400" type="text" placeholder="Label" name="Dissipatore.label" ref={register()} />
         </div>
       </div>
 
