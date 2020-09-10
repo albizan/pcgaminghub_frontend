0357b653d973db080e00e52a9dd3abc9e268c784
unknown <albizan@hotmail.it>
2020-09-10 21:25:13 +0200

    Give null props

--- pages/build/new.tsx
+++ pages/build/new.tsx
@@ -1,9 +1,9 @@
-import NewBuildForm from "../../components/NewBuildForm";
+import BuildForm from "../../components/BuildForm";
 
 export default function NewBuildPage() {
   return (
     <section className="container mx-auto my-16">
-      <NewBuildForm />
+      <BuildForm defaultValues={null} buildId={null} />
     </section>
   );
 }
