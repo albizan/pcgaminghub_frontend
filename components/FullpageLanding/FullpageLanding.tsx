92ba2eab8dff4be3568783f0c1e92e76efacff8d
unknown <albizan@hotmail.it>
2020-09-13 00:05:02 +0200

    Fix font size and scroller padding

--- components/FullpageLanding/FullpageLanding.tsx
+++ components/FullpageLanding/FullpageLanding.tsx
@@ -11,10 +11,8 @@ export default function FullpageLanding() {
     <div className={classNames(styles.background, "h-screen w-full relative bg-red-400")}>
       <div className="absolute inset-0 flex items-center justify-center text-center">
         <div className="mt-12 p-8 text-white text-xl font-thin tracking-wide border-white">
-          <h1>
-            Benvenuto su <span className="block text-5xl font-bold tracking-wide">PC Gaming Hub</span>
-          </h1>
-          <h2>Scegliamo per te le migliori configurazioni per ogni fascia di prezzo</h2>
+          <h1 className="block text-4xl lg:text-5xl font-semibold tracking-wide">PC Gaming Hub</h1>
+          <h2 className="md:text-lg font-thin">Scegliamo per te le migliori configurazioni per ogni fascia di prezzo</h2>
           <div>
             <button className="mt-12 border-2 rounded px-5 py-3 uppercase font-semibold tracking-wide border-white hover:bg-white hover:text-indigo-600 focus:outline-none">
               Mostra Altro
@@ -26,7 +24,7 @@ export default function FullpageLanding() {
             <Navbar landing={true} />
           </Container>
         </div>
-        <div className="absolute bottom-0 w-full flex justify-center pb-20">
+        <div className="absolute bottom-0 w-full flex justify-center pb-6 sm:pb-8 md:pb-8 lg:pb-10">
           <MouseScroll />
         </div>
       </div>
