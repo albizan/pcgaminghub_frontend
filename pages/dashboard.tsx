bb753dd93670c43f548b3c6baf4831037e48a08e
unknown <albizan@hotmail.it>
2020-09-13 09:47:42 +0200

    Improve mobile layout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { BaseBuild } from "../interfaces/BaseBuild.interface";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Container from "../layouts/Container";
 
 export default function Dashboard(props) {
   const [deleteCandidate, setDeleteCandidate] = useState<BaseBuild>(null);
@@ -32,65 +33,70 @@ export default function Dashboard(props) {
     } catch (error) {}
   }
   return (
-    <section className="mx-auto container mt-20">
-      <div className="flex items-center">
-        <h1 className="flex-1 text-3xl">Dashboard</h1>
-        <button className="px-3 py-2 rounded bg-green-400 text-white uppercase text-sm mr-4" onClick={() => router.push("/build/new")}>
-          Nuova Build
-        </button>
-        <button className="px-3 py-2 rounded gradient text-white uppercase text-sm" onClick={handleLogout}>
-          Logout
-        </button>
-      </div>
-      {builds.length > 0 ? (
-        <div className="builds">
-          {builds.map((baseBuild) => {
-            return (
-              <div key={baseBuild.id} className="bg-white rounded shadow-lg my-8 p-4 flex">
-                <h3 className="text-xl font-semibold flex-1">
-                  {baseBuild.name} - {baseBuild.price}€
-                </h3>
-                <div className="flex">
-                  <Link href={`/build/${baseBuild.id}`}>
-                    <button className="mr-2 bg-indigo-600 text-white rounded w-20 text-xs font-semibold">Visualizza</button>
-                  </Link>
-                  <button onClick={() => openDeleteModal(baseBuild)} className="mr-2 bg-red-600 text-white rounded w-20 text-xs font-semibold">
+    <Container>
+      <section className="mt-8 md:mt-12 lg:mt-14">
+        <div className="flex items-center">
+          <h1 className="flex-1 text-xl md:text-2xl lg:text-3xl font-semibold">Dashboard</h1>
+          <button className="text-xs md:text-lg px-2 py-2 rounded bg-green-400 text-white md:uppercase mr-2" onClick={() => router.push("/build/new")}>
+            Nuova Build
+          </button>
+          <button className="text-xs md:text-lg px-2 py-2 rounded gradient text-white md:uppercase" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+        {builds.length > 0 ? (
+          <div className="builds">
+            {builds.map((baseBuild) => {
+              return (
+                <div key={baseBuild.id} className="bg-white rounded shadow-lg my-8 p-4 flex flex-col md:flex-row">
+                  <h3 className="text-2xl text-center md:text-left font-semibold flex-1">
+                    {baseBuild.name} - {baseBuild.price}€
+                  </h3>
+                  <div className="flex justify-center md:justify-end mt-6 md:mt-0 text-xs font-semibold">
+                    <Link href={`/build/${baseBuild.id}`}>
+                      <button className="border border-indigo-600 text-indigo-600 hover:bg-indigo-600 hover:text-white rounded mx-1 w-20">Visualizza</button>
+                    </Link>
+                    <button onClick={() => openDeleteModal(baseBuild)} className="border border-red-600 text-red-600 hover:bg-red-600 hover:text-white rounded mx-1 w-20">
+                      Elimina
+                    </button>
+                    <button
+                      onClick={() => router.push(`/build/edit/${baseBuild.id}`)}
+                      className="border border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white rounded mx-1 w-20"
+                    >
+                      Modifica
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <p className="text-center text-3xl text-gray-700 mt-24">Nessuna configurazione trovata</p>
+        )}
+        {deleteCandidate && (
+          <div className={`${deleteCandidate ? "block" : "hidden"} modal-bg fixed w-full h-screen inset-0 flex justify-center items-center`}>
+            <div className="bg-white rounded-lg px-32 py-16 text-gray-800">
+              <h3 className="text-3xl">{deleteCandidate.name}</h3>
+              <h4 className="text-xl">{deleteCandidate.price}€</h4>
+              <h5 className="text-sm text-gray-500">{deleteCandidate.id}</h5>
+              <p className="text-xl mt-12">Sei sicuro di voler eliminare la configurazione?</p>
+              <div className="flex text-center mt-8">
+                <div className="flex-1">
+                  <button className="bg-red-600 text-lg rounded text-white px-4 py-2 uppercase" onClick={() => handleDelete()}>
                     Elimina
                   </button>
-                  <button onClick={() => router.push(`/build/edit/${baseBuild.id}`)} className="mr-2 bg-yellow-500 text-gray-800 rounded w-20 text-xs font-semibold">
-                    Modifica
+                </div>
+                <div className="flex-1">
+                  <button className="bg-blue-600 text-lg rounded text-white px-4 py-2 uppercase" onClick={() => setDeleteCandidate(null)}>
+                    Annulla
                   </button>
                 </div>
               </div>
-            );
-          })}
-        </div>
-      ) : (
-        <p className="text-center text-3xl text-gray-700 mt-24">Nessuna configurazione trovata</p>
-      )}
-      {deleteCandidate && (
-        <div className={`${deleteCandidate ? "block" : "hidden"} modal-bg fixed w-full h-screen inset-0 flex justify-center items-center`}>
-          <div className="bg-white rounded-lg px-32 py-16 text-gray-800">
-            <h3 className="text-3xl">{deleteCandidate.name}</h3>
-            <h4 className="text-xl">{deleteCandidate.price}€</h4>
-            <h5 className="text-sm text-gray-500">{deleteCandidate.id}</h5>
-            <p className="text-xl mt-12">Sei sicuro di voler eliminare la configurazione?</p>
-            <div className="flex text-center mt-8">
-              <div className="flex-1">
-                <button className="bg-red-600 text-lg rounded text-white px-4 py-2 uppercase" onClick={() => handleDelete()}>
-                  Elimina
-                </button>
-              </div>
-              <div className="flex-1">
-                <button className="bg-blue-600 text-lg rounded text-white px-4 py-2 uppercase" onClick={() => setDeleteCandidate(null)}>
-                  Annulla
-                </button>
-              </div>
             </div>
           </div>
-        </div>
-      )}
-    </section>
+        )}
+      </section>
+    </Container>
   );
 }
 
