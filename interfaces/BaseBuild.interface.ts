f2a159c759a636ba6150c03283e28215457c2948
unknown <albizan@hotmail.it>
2020-09-14 22:28:39 +0200

    Update with new values

--- interfaces/BaseBuild.interface.ts
+++ interfaces/BaseBuild.interface.ts
@@ -2,6 +2,8 @@ export interface BaseBuild {
   id: string;
   price: number;
   name: string;
+  subTitle: string;
+  imageUrl: string;
   cpuBrand: string;
   gpuBrand: string;
   cpuLabel: string;
