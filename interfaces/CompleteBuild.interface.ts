755ddbf6b862b650a7d97bb06941722011c4c697
unknown <albizan@hotmail.it>
2020-09-15 12:25:11 +0200

    Update with new values

--- interfaces/CompleteBuild.interface.ts
+++ interfaces/CompleteBuild.interface.ts
@@ -4,8 +4,10 @@ export interface CompleteBuild {
   id: string;
   price: number;
   name: string;
+  imageUrl: string;
   cpuBrand: string;
   gpuBrand: string;
+  subtitle: string;
   description: string;
   date: string;
   items: Item[];
