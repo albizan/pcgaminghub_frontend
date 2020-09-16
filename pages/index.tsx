e074b2ab1842ec160cf118197f989fd270d994b6
unknown <albizan@hotmail.it>
2020-09-16 18:08:26 +0200

    Change title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>PC Gaming Hub</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FullpageLanding />
