32d89d06ce00cd80f080bcae4a11df017d96f75d
unknown <albizan@hotmail.it>
2020-09-16 18:11:07 +0200

    Increase font size on mobile devices

--- components/FullpageLanding/FullpageLanding.tsx
+++ components/FullpageLanding/FullpageLanding.tsx
@@ -12,10 +12,10 @@ export default function FullpageLanding() {
       <div className="absolute inset-0 flex items-center justify-center text-center">
         <div className="mt-12 p-8 text-white text-xl font-thin tracking-wide border-white">
           <h1 className="block text-3xl md:text-4xl lg:text-5xl font-semibold tracking-wide">PC Gaming Hub</h1>
-          <h2 className="text-sm tracking-tight md:text-lg font-thin">Scegliamo per te le migliori configurazioni per ogni fascia di prezzo</h2>
+          <h2 className="tracking-tight md:text-lg font-thin">Scegliamo per te le migliori configurazioni per ogni fascia di prezzo</h2>
           <div>
             <button className="text-xs md:text-lg mt-12 border-2 rounded px-5 py-3 uppercase font-semibold tracking-wide border-white hover:bg-white hover:text-indigo-600 focus:outline-none">
-              Mostra Altro
+              Entra
             </button>
           </div>
         </div>
