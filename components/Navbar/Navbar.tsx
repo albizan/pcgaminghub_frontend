74bd7f65939a8aa20cc774f02fa9f62292160d6f
unknown <albizan@hotmail.it>
2020-09-17 16:01:38 +0200

    Fix color on standard pages

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import { useState } from "react";
-import styles from "./styles.module.css";
+import Link from "next/link";
 
 export default function Navbar({ landing }) {
   let [isOpen, setIsOpen] = useState(false);
@@ -12,36 +12,43 @@ export default function Navbar({ landing }) {
   return (
     <header className="flex flex-wrap text-gray-700 py-4 lg:py-8 select-none">
       <div className="flex-1 flex justify-between items-center">
-        <a href="#" className={classNames({ "text-white": landing, "text-indigo-600": !landing }, "text-2xl font-semibold tracking-tight")}>
+        <a href="#" className={classNames({ "text-white": landing, "text-gray-900": !landing }, "text-2xl font-semibold tracking-tight")}>
           PC Gaming Hub
         </a>
         <span onClick={toggleNav} className="cursor-pointer lg:hidden block">
-          <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+          <svg
+            className={classNames({ "text-white": landing, "text-gray-900": !landing }, "fill-current")}
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+          >
             <title>menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
           </svg>
         </span>
       </div>
       <div
-        className={classNames({ hidden: !isOpen, block: isOpen, "text-white": landing, "text-indigo-600": !landing }, "lg:flex lg:items-center lg:w-auto w-full")}
+        className={classNames({ hidden: !isOpen, block: isOpen, "text-white": landing, "text-gray-900": !landing }, "lg:flex lg:items-center lg:w-auto w-full")}
         id="menu"
       >
         <nav className="text-right text-lg lg:text-xl lg:text-left">
           <ul className="lg:flex items-center justify-between lg:pt-0">
-            <li className="border-2 border-white p-3 rounded-lg lg:border-0 lg:p-0 my-3 lg:my-0">
-              <a
-                className={classNames(
-                  { "hover:border-white": landing, "hover:border-indigo-600": !landing },
-                  "lg:mt-0 inline-block border-b-2 border-transparent lg:ml-6",
-                )}
-                href="#"
-              >
-                Chi Siamo
-              </a>
+            <li className={classNames({ "border-white": landing, "border-gray-700": !landing }, "border-2 p-3 rounded-lg lg:border-0 lg:p-0 my-3 lg:my-0")}>
+              <Link href="/about-us">
+                <a
+                  className={classNames(
+                    { "hover:border-white": landing, "hover:border-gray-900": !landing },
+                    "lg:mt-0 inline-block border-b-2 border-transparent lg:ml-6",
+                  )}
+                >
+                  Chi Siamo
+                </a>
+              </Link>
             </li>
-            <li className="border-2 border-white p-3 rounded-lg lg:border-0 lg:p-0 my-3 lg:my-0">
+            <li className={classNames({ "border-white": landing, "border-gray-700": !landing }, "border-2 p-3 rounded-lg lg:border-0 lg:p-0 my-3 lg:my-0")}>
               <a
-                className={classNames({ "hover:border-white": landing, "hover:border-indigo-600": !landing }, "inline-block border-b-2 border-transparent lg:ml-6")}
+                className={classNames({ "hover:border-white": landing, "hover:border-gray-900": !landing }, "inline-block border-b-2 border-transparent lg:ml-6")}
                 href="https://t.me/offertecomponenti"
               >
                 Offerte
@@ -50,8 +57,15 @@ export default function Navbar({ landing }) {
             <li className="lg:border-0 lg:p-0 my-3 lg:my-0">
               <a
                 className={classNames(
-                  { "hover:border-white": landing, "hover:border-indigo-600": !landing },
-                  "inline-block bg-gray-100 text-indigo-600 lg:ml-6 rounded-lg px-3 py-2",
+                  {
+                    "hover:border-white": landing,
+                    "hover:border-gray-900": !landing,
+                    "bg-gray-100": landing,
+                    gradient: !landing,
+                    "text-gray-900": landing,
+                    "text-white": !landing,
+                  },
+                  "inline-block lg:ml-6 rounded-lg px-3 py-2",
                 )}
                 target="_blank"
                 href="https://t.me/pcbuildingitaly"
