d652efe751fd2cee4e5924f6c2f457a72690f3c9
unknown <albizan@hotmail.it>
2020-09-17 14:31:27 +0200

    Add telegram icon

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,18 @@
+import { IconContext } from "react-icons";
+import { FaTelegram } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <section className="mt-32">
       <div className="gradient py-20 flex flex-col items-center justify-center">
         <p className="text-white text-lg md:text-xl">&copy; {new Date().getFullYear()} pcgaming.it - All right reserved </p>
+        <IconContext.Provider value={{ color: "white", size: "2.4rem" }}>
+          <div className="icons flex justify-center mt-4">
+            <a target="_blank" href="https://t.me/pcbuildingitaly">
+              <FaTelegram />
+            </a>
+          </div>
+        </IconContext.Provider>
       </div>
     </section>
   );
