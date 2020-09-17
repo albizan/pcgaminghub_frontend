800ca62a82e52668269fd75bc7b8ec6c0eaa8983
unknown <albizan@hotmail.it>
2020-09-17 14:35:46 +0200

    Adjust paddings on small devices

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { FaTelegram } from "react-icons/fa";
 export default function Footer() {
   return (
     <section className="mt-32">
-      <div className="gradient py-20 flex flex-col items-center justify-center">
+      <div className="gradient py-12 md:py-16 lg:py-20 flex flex-col items-center justify-center">
         <p className="text-white text-lg md:text-xl">&copy; {new Date().getFullYear()} pcgaming.it - All right reserved </p>
         <IconContext.Provider value={{ color: "white", size: "2.4rem" }}>
           <div className="icons flex justify-center mt-4">
