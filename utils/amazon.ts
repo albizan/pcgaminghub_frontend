25ae84d92cc7cf6d6ebd6a5d4bfa8b73b283f7aa
unknown <albizan@hotmail.it>
2020-09-19 13:39:26 +0200

    extract asins

--- utils/amazon.ts
+++ utils/amazon.ts
@@ -13,3 +13,17 @@ export function createCartUrl(completeBuild: CompleteBuild, ref: string): string
   cartUrl += `tag=${ref}&AssociateTag=${ref}`;
   return cartUrl;
 }
+
+export function extractAsinFromUrl(url: string): string {
+  // Check if url is not a true url but already a valid asin
+  if (url.length === 10) return url;
+  // Clean url from queries and refs
+  let baseUrl = url.split("?")[0].split("/ref=")[0];
+  if (baseUrl.charAt(baseUrl.length - 1) === "/") {
+    baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+  }
+  // When url is cleaned, ASIN is the latest element of the url string
+  const splittedUrl = baseUrl.split("/");
+  const asin = splittedUrl.slice(-1)[0];
+  return asin;
+}
