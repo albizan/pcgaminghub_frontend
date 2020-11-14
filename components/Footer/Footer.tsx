98a2f7dff83f936eb7d3534cab0aaf00fa7892d6
unknown <albizan@hotmail.it>
2020-11-14 19:21:03 +0100

    fix domain

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <section className="mt-32">
       <div className="gradient py-12 md:py-16 lg:py-20 flex flex-col items-center justify-center">
-        <p className="text-white text-lg md:text-xl">&copy; {new Date().getFullYear()} pcgaming.it - All right reserved </p>
+        <p className="text-white text-lg md:text-xl">&copy; {new Date().getFullYear()} pcgaming.fun - All right reserved </p>
         <IconContext.Provider value={{ color: "white", size: "2.4rem" }}>
           <div className="icons flex justify-center mt-4">
             <a target="_blank" rel="noopener" href="https://t.me/pcbuildingitaly">
