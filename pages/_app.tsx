ce1926dc0b8cf03442861110ade67e188c97f087
unknown <albizan@hotmail.it>
2020-11-15 11:39:58 +0100

    Meta to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-
 import "../styles/globals.css";
 // Import Tailwindcss
 import "../styles/main.css";
@@ -7,9 +5,6 @@ import "../styles/main.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <Component {...pageProps} />;
     </>
   )
