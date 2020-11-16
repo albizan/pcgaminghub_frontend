25a524d39dd9d000aa178bf20d1c6ee89d5ff47e
unknown <albizan@hotmail.it>
2020-11-16 10:28:54 +0100

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+
 import "../styles/globals.css";
 // Import Tailwindcss
 import "../styles/main.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Component {...pageProps} />;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -12,6 +13,23 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+            }}
+          />
         <body>
           <Main />
           <NextScript />
